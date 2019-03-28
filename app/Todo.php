6143fed64aa84e40e4cecf00c4bcae670ebff68c
Rudi Setiawan <rudestewing@gmail.com>
2019-03-28 20:22:34 +0700

    todo model

--- app/Todo.php
+++ app/Todo.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Todo extends Model 
 {
-
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_id');
+    }
 }
